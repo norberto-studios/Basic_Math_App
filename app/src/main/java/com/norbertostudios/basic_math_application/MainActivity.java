33e1c4e608e9073a5c2d0c4827ea78fe590fcc9e
Norberto Fernandez <norbertostudios@gmail.com>
2019-06-28 20:30:06 -0400

    Initial Commit

---
+++ app/src/main/java/com/norbertostudios/basic_math_application/MainActivity.java
@@ -0,0 +1,13 @@
+package com.norbertostudios.basic_math_application;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
